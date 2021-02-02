6440a870e2799098cf9190d01648333b069b5d1f
scm.htethtetoowai@gmail.com <scm.htethtetoowai@gmail.com>
2021-02-02 09:25:13 +0630

    OJTBBL008_login

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+use Hash;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+
+      return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+         
+            return redirect('posts');
+        }
+
+        return redirect('login')->with('error', 'Oppes! You have entered invalid credentials');
+    }
+
+    public function logout() {
+      Auth::logout();
+      return redirect('/');
+    }
+
+  }
